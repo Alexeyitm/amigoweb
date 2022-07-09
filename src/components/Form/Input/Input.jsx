6dc89917b742e4c211b6cc4b3989fb0f3c51e3ac
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-09 19:04:47 +0500

    ref: ref code

--- src/components/Form/Input/Input.jsx
+++ src/components/Form/Input/Input.jsx
@@ -10,9 +10,8 @@ function Input({ type, name, placeholder, input, setInput, valid, textValid }) {
   return (
     <div className="input">
       <label className="input__label" htmlFor={name}>{name}</label>
-      <input className="input__input" type={type} name={name} 
+      <input className="input__input" type={type} name={name}
       id={name} placeholder={placeholder} value={input} onChange={handleChange}/>
-      {/*<span className={"input__error " + (valid && "input__error_hidden")}>{textValid}</span>*/}
       <span className={"input__error " + (valid && "input__error_hidden")}>{input.length ? textValid : ''}</span>
     </div>
   );
