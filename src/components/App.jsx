150514157473e7ca4c3e1981a2014073bd4f57f1
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-07-11 16:19:20 +0500

    docs: add comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,25 +4,28 @@ import Form from './Form/Form';
 
 function App() {
 
+  /* Стейты с данными полей */
   const [isName, setIsName] = useState('');
   const [isEmail, setIsEmail] = useState('');
   const [isPhone, setIsPhone] = useState('');
   const [isLanguage, setIsLanguage] = useState('');
   const [isCheck, setIsCheck] = useState(false);
 
+  /* Стейты с валидностью полей */
   const [isNameValid, setIsNameValid] = useState(false);
   const [isEmailValid, setIsEmailValid] = useState(false);
   const [isPhoneValid, setIsPhoneValid] = useState(false);
   const [isLanguageValid, setIsLanguageValid] = useState(false);
-
   const [isInputsValid, setIsInputsValid] = useState(false);
   const [isFormValid, setIsFormValid] = useState(false);
 
+  /* Валидация имени */
   useEffect(() => {
     setIsNameValid(isName.split('').map(el => /[а-яА-Яa-zA-Z -]/g.test(el)).every(Boolean) &&
       isName.length > 0);
   }, [isName]);
 
+  /* Валидация email */
   useEffect(() => {
     let arr = isEmail.split('@').join('.').split('.');
     setIsEmailValid(arr.length === 3 && arr[0].length >= 1 && arr[1].length >= 1 && arr[2].length >= 2 &&
@@ -30,6 +33,7 @@ function App() {
     );
   }, [isEmail]);
 
+  /* Валидация телефона */
   useEffect(() => {
     setIsPhoneValid((isPhone[0] === "+" || isPhone[0] === "7" || isPhone[0] === "8") &&
       isPhone.replace(/[^0-9]/gim,'').length === 11 &&
@@ -37,10 +41,12 @@ function App() {
     );
   }, [isPhone]);
 
+  /* Валидация языка */
   useEffect(() => {
     setIsLanguageValid(Boolean(isLanguage))
   }, [isLanguage]);
 
+  /* Валидация всех полей */
   useEffect(() => {
     setIsInputsValid(isNameValid && isEmailValid && isPhoneValid && isLanguageValid)
   }, [isNameValid, isEmailValid, isPhoneValid, isLanguageValid]);
